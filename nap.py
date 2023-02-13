1bef26c93a9462a4254548c5788fca7b7ef77cdc
Martin <erdei.martin.2021e@ms.gdszeged.hu>
2023-02-13 09:20:06 +0100

    V1.0

---
+++ nap.py
@@ -0,0 +1,6 @@
+nap=input('Jó a napod(igen,nem)? ')
+if nap == 'igen' or nap == 'Igen' or nap == 'i':
+    print('Az jó!')
+
+elif nap == 'nem' or nap == 'Nem' or nap=='n':
+    print('Az nem jó!')
---
+++ nap2.py
@@ -0,0 +1,9 @@
+nap=input('Jó a napod(igen,nem)? ')
+if nap == 'igen' or nap == 'Igen' or nap == 'i':
+    print('Az jó!')
+
+elif nap == 'nem' or nap == 'Nem' or nap=='n':
+    print('Az nem jó!')
+    
+else:
+    print('Sajnos nem értem a válaszodat!')
